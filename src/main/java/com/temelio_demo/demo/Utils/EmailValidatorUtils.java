1752bdf256da6a346d036989006b5ced4b4ae48a
ashwingarg <ashwin@otpless.com>
2024-06-24 03:30:48 +0530

    adding email regex

--- src/main/java/com/temelio_demo/demo/Utils/EmailValidatorUtils.java
+++ src/main/java/com/temelio_demo/demo/Utils/EmailValidatorUtils.java
@@ -2,14 +2,19 @@ package com.temelio_demo.demo.Utils;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidatorUtils {
 
-    private String EMAIL_REGEX="[a-zA-Z0-9.!#$%&*+=?^_~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+";
+    private static String EMAIL_REGEX="[a-zA-Z0-9.!#$%&*+=?^_~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
     public static boolean isEmailValid(String email) {
         if(StringUtils.isBlank(email)){
             return false;
         }
-        return true;
+        Matcher matcher = EMAIL_PATTERN.matcher(email);
+        return matcher.matches();
     }
 }
