a93b44f5c455bbde68a90c50f3dc288b4ec43ba4
ashwingarg <ashwin@otpless.com>
2024-06-24 02:57:46 +0530

    config changes

--- src/main/java/com/temelio_demo/demo/Config/CorsConfig.java
+++ src/main/java/com/temelio_demo/demo/Config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
